9611628ba60d12afa2a68836c046bf4f8c527bf3
Gideon Weng <wenggy@shanghaitech.edu.cn>
2019-05-11 00:21:19 +0800

    Add: determine winner.

--- gameEngine.m
+++ gameEngine.m
@@ -2,9 +2,16 @@ classdef gameEngine < handle
     
     % properties of poker game engine
     properties
+        % All players
         player_0
         player_1
         player_2
+        % Total card Num = 54
+        cardNum = 54;
+        % winner=-1 -> have not determined winner
+        winner = -1;
+        % determine whether to end the game
+        isEnd = false;
     end
     
     % methods that game engine has:
@@ -25,6 +32,21 @@ classdef gameEngine < handle
                     eg.player_0.role = 1;
                     eg.player_1.role = 1;
                     eg.player_2.role = 0;
+            end
+        end
+        % determine which player is winner
+        function determineWinner(eg)
+            if (eg.player_0.cardNum == 0)
+                eg.winner = 0;
+                eg.isEnd = true;
+            end
+            if (eg.player_1.cardNum == 0)
+                eg.winner = 1;
+                eg.isEnd = true;
+            end
+            if (eg.player_2.cardNum == 0)
+                eg.winner = 2;
+                eg.isEnd = true;
             end
         end
         %
