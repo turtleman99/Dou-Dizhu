e593c75a94540b677307ddb9cc6152ddf1bceb05
Gideon Weng <wenggy@shanghaitech.edu.cn>
2019-05-12 19:30:21 +0800

    Add: player.m.

---
+++ player.m
@@ -0,0 +1,18 @@
+classdef player < handle
+    
+    % properties of poker game engine
+    properties
+        avatar = 'icon_default.png';
+        role = -1;      % -1-defult, 0-landlord, 1-peasant;
+        cards = {};
+        cardNum = 0;
+        selectNum = 0;  % remember to reset
+        isActive = false;
+        currUI;         % its own UI
+    end
+    
+    % methods that game engine has:
+    methods
+        
+    end
+end
