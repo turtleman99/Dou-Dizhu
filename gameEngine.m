b8fe6b4ce3993f3565ccfa44f4d6e39da4f327d1
Gideon Weng <wenggy@shanghaitech.edu.cn>
2019-05-12 10:06:49 +0800

    Add: dsistributeCards

--- gameEngine.m
+++ gameEngine.m
@@ -6,20 +6,26 @@ classdef gameEngine < handle
         player_0
         player_1
         player_2
+        
+        landlord
+        whoseTurn = -1;
+        % import cards data
+        cardsData = transpose(struct2cell(jsondecode(fileread('cards.json'))));
         % Total card Num = 54
         cardNum = 54;
         % winner=-1 -> have not determined winner
         winner = -1;
         % determine whether to end the game
         isEnd = false;
+        
     end
     
     % methods that game engine has:
     methods
         % In 0 stage, random assign Role; -1-defult, 0-landlord, 1-peasant;
         function assignRole(eg)
-            indx = randi(2);
-            switch indx
+            eg.landlord = randi(3)-1;
+            switch eg.landlord
                 case 0
                     eg.player_0.role = 0;
                     eg.player_1.role = 1;
@@ -39,19 +45,47 @@ classdef gameEngine < handle
             if (eg.player_0.cardNum == 0)
                 eg.winner = 0;
                 eg.isEnd = true;
-            end
-            if (eg.player_1.cardNum == 0)
+            elseif (eg.player_1.cardNum == 0)
                 eg.winner = 1;
                 eg.isEnd = true;
-            end
-            if (eg.player_2.cardNum == 0)
+            elseif (eg.player_2.cardNum == 0)
                 eg.winner = 2;
                 eg.isEnd = true;
             end
         end
+        % Shuffle and distribute cards
+        function dsistributeCards(eg)
+            % Shuffle
+            order = randperm(54);
+            % distribute to all players
+            for i = 1:17
+                indx = order(i);
+                eg.player_0.cards = [eg.player_0.cards, eg.cardsData{indx}];
+            end
+            for i = 18:34
+                indx = order(i);
+                eg.player_1.cards = [eg.player_1.cards, eg.cardsData{indx}];
+            end
+            for i = 35:51
+                indx = order(i);
+                eg.player_2.cards = [eg.player_2.cards, eg.cardsData{indx}];
+            end
+            % distribute to landlord
+            if (eg.landlord == 0)
+                eg.player_0.cards = [eg.player_0.cards, eg.cardsData{52}, eg.cardsData{53}, eg.cardsData{54}];
+            elseif (eg.landlord == 1)
+                eg.player_1.cards = [eg.player_1.cards, eg.cardsData{52}, eg.cardsData{53}, eg.cardsData{54}];
+            elseif (eg.landlord == 2)
+                eg.player_2.cards = [eg.player_2.cards, eg.cardsData{52}, eg.cardsData{53}, eg.cardsData{54}];
+            end
+        end
         %
         
-        % End fame with following process
+        % start game with following process
+        function startGame(eg)
+            
+        end
+        % End game with following process
         function endGame(eg)
             
         end
